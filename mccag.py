f194ab2219136af0a3ec51ae7d6c6460cc006e32
Natsusome Keishi <44630360+Natsusomekeishi@users.noreply.github.com>
2024-07-25 04:19:18 +0800

    Update mccag.py

--- mccag.py
+++ mccag.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request, send_file, render_template, url_for
+from flask import Flask, request, send_file, render_template, url_for, jsonify
 import requests
 import base64
 import json
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageOps
 from io import BytesIO
 import os
 import time
 
 app = Flask(__name__)
 CACHE_DIR = 'static/cache'
-CACHE_DURATION = 24 * 60 * 60  # 24 小时的秒数
+CACHE_DURATION = 10 * 60  # 10 分钟的秒数
 
 if not os.path.exists(CACHE_DIR):
     os.makedirs(CACHE_DIR)
@@ -61,12 +61,21 @@ def download_image(url):
 def create_image_with_paste(source_image, target_image_path, canvas_size, operations):
     """创建图像并保存到目标路径。"""
     canvas = Image.new('RGBA', canvas_size, (255, 255, 255, 0))
-    resized_source_image = source_image.resize((128, 128), Image.NEAREST)
+    skin_size = source_image.size
+
+    # 根据皮肤尺寸选择缩放
+    if skin_size == (64, 32):
+        resized_source_image = source_image.resize((128, 64), Image.NEAREST)
+    else:
+        resized_source_image = source_image.resize((128, 128), Image.NEAREST)
 
     for operation in operations:
-        crop_box, scale_factor, paste_position = operation
+        crop_box, scale_factor, paste_position, *mirror = operation
 
         cropped_image = resized_source_image.crop(crop_box)
+        if mirror:
+            cropped_image = ImageOps.mirror(cropped_image)
+            
         new_size = (int(cropped_image.size[0] * scale_factor), int(cropped_image.size[1] * scale_factor))
         bordered_size = (new_size[0] + 30, new_size[1] + 30)
         bordered_image = Image.new('RGBA', bordered_size, (0, 0, 0, 0))
@@ -94,7 +103,6 @@ def get_cache_filename(username):
 
 def is_cache_valid(filename):
     """检查缓存是否有效。"""
-
     ignore_filename = '923ed5ce249a4cd3ac7d23e6797b939c.png'
     
     if os.path.basename(filename) == ignore_filename:
@@ -132,20 +140,33 @@ def index():
                 if skin_image:
                     target_image_path = cache_filename
                     canvas_size = (1000, 1000)  # 新画布的大小
-                    operations = [
-                        ((8, 40, 16, 64), 8.375, (434,751)),
-                        ((8, 72, 16, 96), 9.375, (428,737)),
-                        ((40, 104, 48, 128), 8.375, (505,751)),
-                        ((8, 104, 16, 128), 9.375, (503,737)),
-                        ((86, 40, 92, 64), 8.167, (388,561)),
-                        ((88, 72, 94, 96), 9.5, (382,538)),
-                        ((74, 104, 80, 128), 8.167, (566,561)),
-                        ((104, 104, 110, 128), 9.5, (564,538)),
-                        ((40, 40, 56, 64), 8.0625, (437,561)),
-                        ((40, 72, 56, 96), 8.6575, (432,555)),
-                        ((16, 16, 32, 32), 26.875, (287,131)),
-                        ((80, 16, 96, 32), 30.8125, (254,107)),
-                    ]
+                    skin_size = skin_image.size
+
+                    if skin_size == (64, 32):
+                        operations = [
+                            ((8, 40, 16, 64), 8.375, (434,751)), # 右腿
+                            ((8, 40, 16, 64), 8.375, (505,751), True), # 左腿，使用右腿镜像
+                            ((86, 40, 92, 64), 8.167, (388,561)), # 右臂
+                            ((86, 40, 92, 64), 8.167, (566,561), True), # 左臂，使用右臂镜像
+                            ((40, 40, 56, 64), 8.0625, (437,561)), # 躯干
+                            ((16, 16, 32, 32), 26.875, (287,131)), # 头部
+                            ((80, 16, 96, 32), 30.8125, (254,107)), # 头部外层
+                        ]
+                    else:
+                        operations = [
+                            ((8, 40, 16, 64), 8.375, (434,751)),
+                            ((8, 72, 16, 96), 9.375, (428,737)),
+                            ((40, 104, 48, 128), 8.375, (505,751)),
+                            ((8, 104, 16, 128), 9.375, (503,737)),
+                            ((86, 40, 92, 64), 8.167, (388,561)),
+                            ((88, 72, 94, 96), 9.5, (382,538)),
+                            ((74, 104, 80, 128), 8.167, (566,561)),
+                            ((104, 104, 110, 128), 9.5, (564,538)),
+                            ((40, 40, 56, 64), 8.0625, (437,561)),
+                            ((40, 72, 56, 96), 8.6575, (432,555)),
+                            ((16, 16, 32, 32), 26.875, (287,131)),
+                            ((80, 16, 96, 32), 30.8125, (254,107)),
+                        ]
 
                     create_image_with_paste(skin_image, target_image_path, canvas_size, operations)
                     image_url = url_for('static', filename=f'cache/{os.path.basename(target_image_path)}')
@@ -174,20 +195,33 @@ def mc_avatar_api(username):
         if skin_image:
             target_image_path = cache_filename
             canvas_size = (1000, 1000)  # 新画布的大小
-            operations = [
-                ((8, 40, 16, 64), 8.375, (434,751)),
-                ((8, 72, 16, 96), 9.375, (428,737)),
-                ((40, 104, 48, 128), 8.375, (505,751)),
-                ((8, 104, 16, 128), 9.375, (503,737)),
-                ((86, 40, 92, 64), 8.167, (388,561)),
-                ((88, 72, 94, 96), 9.5, (382,538)),
-                ((74, 104, 80, 128), 8.167, (566,561)),
-                ((104, 104, 110, 128), 9.5, (564,538)),
-                ((40, 40, 56, 64), 8.0625, (437,561)),
-                ((40, 72, 56, 96), 8.6575, (432,555)),
-                ((16, 16, 32, 32), 26.875, (287,131)),
-                ((80, 16, 96, 32), 30.8125, (254,107)),
-            ]
+            skin_size = skin_image.size
+
+            if skin_size == (64, 32):
+                operations = [
+                    ((8, 40, 16, 64), 8.375, (434,751)), # 右腿
+                    ((8, 40, 16, 64), 8.375, (505,751), True), # 左腿，使用右腿镜像
+                    ((86, 40, 92, 64), 8.167, (388,561)), # 右臂
+                    ((86, 40, 92, 64), 8.167, (566,561), True), # 左臂，使用右臂镜像
+                    ((40, 40, 56, 64), 8.0625, (437,561)), # 躯干
+                    ((16, 16, 32, 32), 26.875, (287,131)), # 头部
+                    ((80, 16, 96, 32), 30.8125, (254,107)), # 头部外层
+                ]
+            else:
+                operations = [
+                    ((8, 40, 16, 64), 8.375, (434,751)),
+                    ((8, 72, 16, 96), 9.375, (428,737)),
+                    ((40, 104, 48, 128), 8.375, (505,751)),
+                    ((8, 104, 16, 128), 9.375, (503,737)),
+                    ((86, 40, 92, 64), 8.167, (388,561)),
+                    ((88, 72, 94, 96), 9.5, (382,538)),
+                    ((74, 104, 80, 128), 8.167, (566,561)),
+                    ((104, 104, 110, 128), 9.5, (564,538)),
+                    ((40, 40, 56, 64), 8.0625, (437,561)),
+                    ((40, 72, 56, 96), 8.6575, (432,555)),
+                    ((16, 16, 32, 32), 26.875, (287,131)),
+                    ((80, 16, 96, 32), 30.8125, (254,107)),
+                ]
 
             create_image_with_paste(skin_image, target_image_path, canvas_size, operations)
             return send_file(target_image_path)
