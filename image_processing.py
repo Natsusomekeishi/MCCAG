c40c4258270c1c1bf7a33168c42b576e8966af24
Kyle Ma <m2069134445@icloud.com>
2024-07-28 14:18:18 +0800

    使其支持上传文件生成头像

--- image_processing.py
+++ image_processing.py
@@ -105,3 +105,41 @@ def generate_image(username, avatar_type):
         return cache_filename, url_for('static', filename=f'cache/{os.path.basename(cache_filename)}')
 
     return None, None
+
+
+def generate_image_from_skin_file(skin_file, avatar_type):
+    cache_filename = os.path.join(CACHE_DIR, f'{skin_file.filename}.png')
+
+    if is_cache_valid(cache_filename, CACHE_DURATION):
+        return cache_filename, url_for('static', filename=f'cache/{os.path.basename(cache_filename)}')
+
+    skin_image = Image.open(skin_file)
+
+    if skin_image:
+        canvas_size = (1000, 1000)
+
+        if avatar_type == 'head':
+            operations = [
+                ((16, 16, 32, 32), 42, (165, 155)),
+                ((80, 16, 96, 32), 45, (140, 135)),
+            ]
+        else:
+            operations = [
+                ((8, 40, 16, 64), 8.375, (434, 751)),
+                ((8, 72, 16, 96), 9.375, (428, 737)),
+                ((40, 104, 48, 128), 8.375, (505, 751)),
+                ((8, 104, 16, 128), 9.375, (503, 737)),
+                ((86, 40, 92, 64), 8.167, (388, 561)),
+                ((88, 72, 94, 96), 9.5, (382, 538)),
+                ((74, 104, 80, 128), 8.167, (566, 561)),
+                ((104, 104, 110, 128), 9.5, (564, 538)),
+                ((40, 40, 56, 64), 8.0625, (437, 561)),
+                ((40, 72, 56, 96), 8.6575, (432, 555)),
+                ((16, 16, 32, 32), 26.875, (287, 131)),
+                ((80, 16, 96, 32), 30.8125, (254, 107)),
+            ]
+
+        create_image_with_paste(skin_image, cache_filename, canvas_size, operations)
+        return cache_filename, url_for('static', filename=f'cache/{os.path.basename(cache_filename)}')
+
+    return None, None
--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request, send_file, render_template, jsonify
 import os
-from image_processing import generate_image
+from image_processing import generate_image_from_skin_file, generate_image
 from uuid_fetch import clean_old_cache_files
 
 app = Flask(__name__)
@@ -21,11 +21,20 @@ def before_request():
 def index():
     image_url = None
     if request.method == 'POST':
-        username = request.form.get('username')
-        if not username:
-            return "玩家名称不能为空", 400
+        input_type = request.form.get('input_type')
+        avatar_type = request.form.get('avatar_type')
 
-        cache_filename, image_url = generate_image(username, 'full')
+        if input_type == 'username':
+            username = request.form.get('username')
+            if not username:
+                return "玩家名称不能为空", 400
+            cache_filename, image_url = generate_image(username, avatar_type)
+        elif input_type == 'upload':
+            skin_file = request.files.get('skin')
+            if not skin_file:
+                return "皮肤文件不能为空", 400
+            cache_filename, image_url = generate_image_from_skin_file(skin_file, avatar_type)
+
         if cache_filename is None:
             return "无法生成玩家图像", 500
 
@@ -43,6 +52,20 @@ def mc_avatar_api_head(username):
 @app.route('/mc-avatar-api/full/<username>', methods=['GET'])
 def mc_avatar_api_full(username):
     cache_filename, _ = generate_image(username, 'full')
+    if cache_filename:
+        return send_file(cache_filename)
+    return jsonify({"error": "无法生成玩家图像"}), 500
+
+
+@app.route('/mc-avatar-api/upload', methods=['POST'])
+def mc_avatar_api_upload():
+    skin_file = request.files.get('skin')
+    avatar_type = request.form.get('avatar_type')
+
+    if not skin_file:
+        return jsonify({"error": "皮肤文件不能为空"}), 400
+
+    cache_filename, _ = generate_image_from_skin_file(skin_file, avatar_type)
     if cache_filename:
         return send_file(cache_filename)
     return jsonify({"error": "无法生成玩家图像"}), 500
